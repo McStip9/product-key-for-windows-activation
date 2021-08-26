46ca7eda58bd29b04a57a1647519c49938b08db0
McStip9 <73377369+McStip9@users.noreply.github.com>
2021-08-26 14:49:13 +0800

    Delete Windows 10 directory

--- Windows 10/Enterprise.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pause
-slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
-slmgr /skms kms8.msguides.com
-slmgr /ato
-exit
--- Windows 10/Home Single Language.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-pause
-slmgr /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
-slmgr /skms kms8.msguides.com
-slmgr /ato
-exit
