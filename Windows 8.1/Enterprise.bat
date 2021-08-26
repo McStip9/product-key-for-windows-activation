bbf4a02e21099a78fea7621d981f74b1baecd954
McStip9 <73377369+McStip9@users.noreply.github.com>
2021-08-26 14:49:34 +0800

    Delete Windows 8.1 directory

--- Windows 8.1/Enterprise.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-slmgr /ipk MHF9N-XY6XB-WVXMC-BTDCT-MKKG7
-slmgr /skms s8.now.im
-slmgr /ato
-exit
--- Windows 8.1/Pro.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-slmgr /ipk GCRJD-8NW9H-F2CDX-CCM8D-9D6T9
-slmgr /skms s8.now.im
-slmgr /ato
-exit
